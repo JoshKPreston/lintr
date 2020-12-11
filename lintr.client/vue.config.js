35cc26b7f71b7b9cd80b3747d4717f1439b6e845
Josh K Preston <kanemetal@gmail.com>
2020-12-10 20:49:48 -0700

    'init'

---
+++ lintr.client/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+ // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  publicPath: '/',
+  outputDir: '../lintr.server/client',
+  productionSourceMap: false,
+  transpileDependencies: ['@bcwdev/auth0provider-client']
+}
