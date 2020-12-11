25f5f316953e92434500de2795db6fab010d6d58
Josh K Preston <kanemetal@gmail.com>
2020-12-10 21:15:38 -0700

    'deployment'

--- lintr.client/src/AuthConfig.js
+++ lintr.client/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'bcwclassroom.us.auth0.com'
+export const audience = 'https://lintr/api'
+export const clientId = 'QEEEANxrBGwtVxVbWLVxU2xfodeQ1dDY'
